ece55e145af032c88e59d83f9ca447b543a1842d
instafluff <instaflufftv@gmail.com>
2019-05-14 20:31:40 -0700

    Build 1.0.10

--- dist/comfy.js
+++ dist/comfy.js
@@ -1,5 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-// Comfy.JS v1.0.9
+// Comfy.JS v1.0.10
 var tmi = require( "tmi.js" );
 
 var mainChannel = "";
@@ -8,7 +8,7 @@ var comfyJS = {
   isDebug: false,
   chatModes: {},
   version: function() {
-    return "1.0.9";
+    return "1.0.10";
   },
   onCommand: function( user, command, message, flags, extra ) {
     if( comfyJS.isDebug ) {
@@ -120,6 +120,9 @@ var comfyJS = {
   },
   Init: function( username, password, channels, isDebug ) {
     channels = channels || [ username ];
+    if( typeof channels === 'string' || channels instanceof String ) {
+      channels = [ channels ];
+    }
     if( !Array.isArray( channels ) ) {
       throw new Error( "Channels is not an array" );
     }
