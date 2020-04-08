11d6ac90576b5e413705b2287793cc5d57562d08
Dominique Charlebois <domo.charlebois@gmail.com>
2020-04-08 08:24:51 -0700

    Update Version in ComfyJS

--- dist/comfy.js
+++ dist/comfy.js
@@ -1,5 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-// Comfy.JS v@VERSION
+// Comfy.JS v1.0.16
 var tmi = require( "tmi.js" );
 
 // User and global timestamp store
@@ -65,7 +65,7 @@ var comfyJS = {
   isDebug: false,
   chatModes: {},
   version: function() {
-    return "@VERSION";
+    return "1.0.16";
   },
   onError: function( error ) {
     console.error( "Error:", error );
