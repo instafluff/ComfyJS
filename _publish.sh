65da81b9900552b9c5efbc6e14d797c05758d58f
instafluff <instaflufftv@gmail.com>
2020-05-04 05:16:25 -0700

    Updated Publish Script

--- _publish.sh
+++ _publish.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "Updating version"
 npm version patch
-echo "Pushing to GitHub"
-git push
 echo "Building..."
 npm run build
+echo "Committing..."
+git commit -a -m "Latest Build"
+echo "Pushing to GitHub"
+git push
 echo "Publishing comfy.js"
 npm publish
 # --- NOTE: comfyjs is too similar to comfy.js so NPM returns an error if published ---
