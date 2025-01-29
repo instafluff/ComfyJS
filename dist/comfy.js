90c72e56e0dec3a99b1089a71668a99b05440494
instafluff <instaflufftv@gmail.com>
2025-01-29 22:43:59 +0000

    Latest Version Build

--- dist/comfy.js
+++ dist/comfy.js
@@ -1,5 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-// Comfy.JS v1.1.23
+// Comfy.JS v1.1.24
 var tmi = require( "tmi.js" );
 var fetch = require( "node-fetch" );
 var NodeSocket = require( "ws" );
@@ -155,7 +155,7 @@ async function subscribeToEventAsync( type, version, clientId, userId, password,
         }
       } )
     }).then( r => r.text() );
-    console.log( type, version, subscriptionResult );
+    // console.log( type, version, subscriptionResult );
     return true;
   }
   catch( error ) {
@@ -622,7 +622,7 @@ var comfyJS = {
   useEventSub: true, // set to false to use PubSub
   chatModes: {},
   version: function() {
-    return "1.1.23";
+    return "1.1.24";
   },
   onError: function( error ) {
     console.error( "Error:", error );
